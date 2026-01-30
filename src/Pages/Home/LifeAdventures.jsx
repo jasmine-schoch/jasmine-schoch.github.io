253a64e298c37811979a4f33aeeb6f54f3fb9af0
Jasmine Schoch <jasmineyschoch@gmail.com>
2026-01-30 00:39:11 -0500

    created new blank

--- src/Pages/Home/LifeAdventures.jsx
+++ src/Pages/Home/LifeAdventures.jsx
@@ -1,9 +1,15 @@
 export default function LifeAdventures(){
     return(
-        <div>
-            <h1>Coming soon!!</h1>
-            <h1>Coming soon!!</h1>
-            <h1>Coming soon!!</h1>
-        </div>
+        <section className="life-adventures--section">
+            <div className="life-adventures--container">
+                <p className="life-adventures--description">
+                    I wanted to make this website a little different from the typical CS kid glorified-digital-résumé-as-personal-website (no offense) by sharing more of my interests and personality, offering more insight into my life. The plan is to make this page into a blog and/or reflections page (inspired by the UW honors portfolio) but I've yet gotten around to coding it. In the meantime, checkout <a href="https://www.instagram.com/molihuacha.j/" target="_blank" rel="noopener noreferrer" className="life-adventures--link">@molihuacha.j</a> on Instagram if you want to see what adventures I'm up to lately! (Spoiler alert, it's a lot of dance videos)
+                </p>
+
+                <p className="life-adventures--description">
+                    A couple of other windows to peek into my life: <a href="https://strava.app.link/SUMito9zi0b" target="_blank" rel="noopener noreferrer" className="life-adventures--link">Strava</a> and <a href="https://goodreads.com/user/show/130119728" target="_blank" rel="noopener noreferrer" className="life-adventures--link">Goodreads</a>
+                </p>
+            </div>
+        </section>
     );
 }
